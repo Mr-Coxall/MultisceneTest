d57f54d22d194266ac076562f0e3730cb0d4a27f
cclauss <cclauss@bluewin.ch>
2016-01-06 17:46:36 +0100

    Create MultiScene.py
    Does .setup() first scene too.

---
+++ MultiScene.py
@@ -0,0 +1,27 @@
+# Quick-and-dirty demo of how to run multiple scenes
+# in Pythonista; The MultiScene class is basically a
+# wrapper for another scene and forwards all events
+# to the currently-active scene, which can be changed
+# with the switch_scene method.
+
+from scene import Scene
+
+class MultiScene(Scene):
+    def __init__(self, start_scene):
+        self.switch_scene(start_scene)
+
+    def switch_scene(self, new_scene):
+        self.active_scene = new_scene
+        new_scene.setup()
+
+    def draw(self):
+        self.active_scene.draw()
+
+    def touch_began(self, touch):
+        self.active_scene.touch_began(touch)
+
+    def touch_moved(self, touch):
+        self.active_scene.touch_moved(touch)
+
+    def touch_ended(self, touch):
+        self.active_scene.touch_ended(touch)
